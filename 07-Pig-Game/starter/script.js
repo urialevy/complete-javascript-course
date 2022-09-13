786c4e7b0c7c8ce7ee205b722e783573ba9b3be4
Uria Levy <uria.levy@messagemedia.com>
2022-09-13 21:23:06 +0200

    added score hold functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 // selecting elements
-const score0 = document.getElementById('score--0');
-const score1 = document.getElementById('score--1');
+let score0 = document.getElementById('score--0');
+let score1 = document.getElementById('score--1');
 var diceImg = document.querySelector('.dice');
 diceImg.classList.add('hidden');
 const btnRoll = document.querySelector('.btn--roll');
@@ -15,6 +15,8 @@ const player1 = document.querySelector('.player--1');
 let activePlayer = player0;
 let result = 0;
 let currentScore = 0;
+let score0El = 0;
+let score1El = 1;
 score0.textContent = 0;
 score1.textContent = 0;
 
@@ -37,6 +39,26 @@ btnRoll.addEventListener('click', function () {
     changeActivePlayer();
   }
 });
+
+// hold button
+
+btnHold.addEventListener('click', function () {
+  if (activePlayer == player0) {
+    score0El += currentScore;
+    score0.textContent = score0El;
+    currentScore = 0;
+    current0El.textContent = currentScore;
+    changeActivePlayer();
+  } else {
+    score1El += currentScore;
+    score1.textContent = score1El;
+    currentScore = 0;
+    current1El.textContent = currentScore;
+    changeActivePlayer();
+  }
+});
+
+// toggles active player
 function changeActivePlayer() {
   if (activePlayer == player0) {
     player0.classList.remove('player--active');
